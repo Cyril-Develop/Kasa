c14abb59ac0045d793acc0532a47bb0559826523
Cyril Bourdon <cyril.bourdon@hotmail.fr>
2024-01-27 21:21:29 +0100

    fin

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 export default function Card({id, title, cover}) {
 
 	return (
-		<Link to={`/accomodation/${id}`} className="gallery_card">
+		<Link to={`/kasa/accomodation/${id}`} className="gallery_card">
 			<img src={cover} alt={title} />
 			<h3>{title}</h3>	
 		</Link>
