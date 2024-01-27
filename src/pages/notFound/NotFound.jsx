48b5e48e7ec4cb5083bf868d7277be2ca3a87511
Cyril Bourdon <cyril.bourdon@hotmail.fr>
2024-01-27 10:37:49 +0100

    modif text 404

--- src/pages/notFound/NotFound.jsx
+++ src/pages/notFound/NotFound.jsx
@@ -8,7 +8,7 @@ export default function NotFound() {
 			<Header />
 			<div className="notFound_infos">
 				<h1 className='notFound_infos_title'>404</h1>
-				<p className='notFound_infos_content'>La page que vous recherchez n'existe pas.</p>
+				<p className='notFound_infos_content'>Oups! La page que vous demandez n'existe pas.</p>
 			</div>
 				<Link className='notFound_infos_return' to='/Kasa'>Retourner sur la page d'accueil</Link>
 		</div>
